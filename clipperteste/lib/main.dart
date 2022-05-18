ac7d84dbd8967cf336b1617975bddd1425b6e702
rayenealmeida <rayene@devartbots.com.br>
2022-05-18 15:30:04 -0300

    componetização

--- clipperteste/lib/main.dart
+++ clipperteste/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'MyApp.dart';
 
 void main() => runApp(const MaterialApp(
   debugShowCheckedModeBanner: false,
@@ -6,49 +7,3 @@ void main() => runApp(const MaterialApp(
 ));
 
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: <Widget>[
-          ClipPath(
-            clipper: MyClipper(),
-            child: Container(
-              height: 320,
-              decoration: BoxDecoration(
-                color: Colors.purple[300],
-              ),
-            ),
-          )
-        ],
-      )
-    );
-  }
-}
-
-class MyClipper extends CustomClipper<Path>{
-  @override
-  Path getClip(Size size) {
-    var path = new Path();
-    path.lineTo(0, 250);
-    path.lineTo(250, 0);
-    path.lineTo(size.width-500, size.height-500);
-
-    return path;
-
-  }
-
-  @override
-  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
-    // TODO: implement shouldReclip
-    return false;
-  }
-
-}
